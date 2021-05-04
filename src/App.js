8355371d491b49a006f27696adfd6d304d40aff4
Anna Tielke <anna_tielke@gmx.de>
2021-05-04 17:53:08 +0200

    Iteration 2 solved

--- src/App.js
+++ src/App.js
@@ -8,15 +8,28 @@ class App extends Component {
       contacts: contacts.slice(0, 5),
    };
 
+   handleAdd = () => {
+      let randomIndex = Math.floor(Math.random() * contacts.length);
+      let elem = contacts[randomIndex];
+      //this.setState overwrites the contacts with the new Array
+      this.setState({
+         contacts: [elem, ...this.state.contacts],
+      });
+   };
+
    render() {
       const { contacts } = this.state;
       return (
          <ul>
+            <button onClick={this.handleAdd}>Add</button>
             {contacts.map((singleContact) => {
                return (
                   <>
                      <li>
-                        <img src={singleContact.pictureUrl}></img>
+                        <img
+                           style={{ width: "50px" }}
+                           src={singleContact.pictureUrl}
+                        ></img>
                      </li>
                      <li>{singleContact.name} </li>
                      <li>{singleContact.popularity} </li>
